2a780d513bcd1ca6de4e971852bb8dcf9eb5955b
elkassir hamza <elkassir.hamza05@gmail.com>
2020-07-21 14:09:19 +0100

    footer responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <script href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="../dist/css/main.css">
+    <title>INAS Tanger</title>
+
 </head>
+
 <body>
 <!-- navbar -->
 
@@ -15,13 +21,104 @@
 
 
 
-<!-- footer -->
+    <!--  FOOTER  -->
+
+    <div class="footer">
+        <div class="footer__content">
+
+            
+            <div class="footer__content__items">
+                <img src="../dist/img/inas_logo.jpeg" alt="" class="footer__content__items--logo">
+                <!-- <h1>INAS Tanger</h1>
+                <p>L’INAS - Institut National de l’Action Sociale</p> -->
+            </div>
+
+            
+
+            <div class="footer__content__items">
+                <h3>Liens</h3>
+                <div class="footer__content__items--borders"></div>
+                <!--les liens -->
+                <ul>
+                    <a href="#">
+                        <li>HOME</li>
+                    </a>
+                    <a href="#">
+                        <li>PRESENTATION DE L’INAS</li>
+                    </a>
+                    <a href="#">
+                        <li>MOT DE DIRECTRICE</li>
+                    </a>
+                    <a href="#">
+                        <li>MISSION</li>
+                    </a>
+                    <a href="#">
+                        <li>HISTORIQUE</li>
+                    </a>
+                    <a href="#">
+                        <li>DEPARTEMENTS</li>
+                    </a>
+                </ul>
+            </div>
+
+            
+
+            <div class="footer__content__items">
+                <h3>Accès rapide</h3>
+                <div class="footer__content__items--borders"></div>
+                <!--les liens pour l'accès rapide -->
+                <ul>
+                    <a href="#">
+                        <li>AGENDA</li>
+                    </a>
+                    <a href="#">
+                        <li>EVENEMENTS</li>
+                    </a>
+                    <a href="#">
+                        <li>ACTIVITES</li>
+                    </a>
+                    <a href="#">
+                        <li>AVIS</li>
+                    </a>
+                    <a href="#">
+                        <li>NEWS</li>
+                    </a>
+                    <a href="#">
+                        <li>ESPACE PRESSE</li>
+                    </a>
+                </ul>
+            </div>
+
+            <!--  contact us -->
+            <div class="footer__content__items">
+                <h3>Contact us</h3>
+                <div class="footer__content__items--borders"></div>
+                <ul>
+                    <li><i class="fa fa-phone" aria-hidden="true"></i>0539940971</li>
+                    <li><i class="fa fa-envelope" aria-hidden="true"></i>xyz@gmail.com</li>
+                    <li><i class="fas fa-map-marker-alt" aria-hidden="true"></i>INAS, Rue Hariri B.P 1168, Tanger, Maroc
+                    </li>
+
+                </ul>
+
+                <!--   social media  -->
+                <div class="footer__content__items--socialmed">
+                    <a href="#"><i class="fab fa-linkedin"></i></a>
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-google-plus-square"></i></a>
+                </div>
+            </div>
+        </div>
+
+        <!--    Bottom footer  -->
+        <div class="footer__bottom">
+            Copyright &copy; Digital24 2020.
+        </div>
+    </div>
 
 
-
-
-
-
+    <!--   FOOTER END -->
 
 </body>
+
 </html>
